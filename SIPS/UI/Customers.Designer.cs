9c2479b8115cff298bd0a7998c2c8fc516b02e85
Ahmad Hassan Tanseel <hassantanseelahmad@gmail.com>
2026-03-27 01:34:45 +0500

    Customer Dashboard Done

---
+++ SIPS/UI/Customers.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace SIPS.UI
+{
+    partial class Customers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            dataGridView2 = new DataGridView();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(266, 744);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(29, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(217, 34);
+            button2.TabIndex = 1;
+            button2.Text = "My Bookings";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 241);
+            button1.Name = "button1";
+            button1.Size = new Size(217, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Browse Magazines";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.ItemSize = new Size(0, 1);
+            tabControl1.Location = new Point(266, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(912, 744);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Location = new Point(4, 5);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(904, 735);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Browse Magazines";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button4);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(dataGridView2);
+            tabPage2.Location = new Point(4, 5);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(904, 735);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "My Bookings";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 58);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(904, 610);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.Location = new Point(275, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(369, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Book Your Magazines Here";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(357, 693);
+            button3.Name = "button3";
+            button3.Size = new Size(178, 34);
+            button3.TabIndex = 2;
+            button3.Text = " Book Selected";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(357, 688);
+            button4.Name = "button4";
+            button4.Size = new Size(178, 34);
+            button4.TabIndex = 5;
+            button4.Text = "Cancel Booking";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label2.Location = new Point(317, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(189, 38);
+            label2.TabIndex = 4;
+            label2.Text = "My Bookings";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(0, 53);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.Size = new Size(904, 610);
+            dataGridView2.TabIndex = 3;
+            // 
+            // Customers
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1178, 744);
+            Controls.Add(tabControl1);
+            Controls.Add(panel1);
+            Name = "Customers";
+            Text = "Customers";
+            Load += Customers_Load;
+            panel1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private DataGridView dataGridView2;
+    }
+}
---
+++ SIPS/UI/Customers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIPS.UI
+{
+    public partial class Customers : Form
+    {
+        public Customers()
+        {
+            InitializeComponent();
+        }
+
+        private void Customers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
