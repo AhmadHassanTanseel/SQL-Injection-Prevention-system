a33ceeb8e6d12923f64114a3cbfbf6a4a859071c
Ahmad Hassan Tanseel <hassantanseelahmad@gmail.com>
2026-04-23 09:05:50 +0500

    Calll Stack Fixed

--- SIPS/UI/Admin.Designer.cs
+++ SIPS/UI/Admin.Designer.cs
@@ -627,6 +627,7 @@
         private Button button4;
         private Button button9;
         private Button button8;
+        //private TextBox 
         private DataGridView dataGridView1;
         private Button button11;
         private Button button12;
@@ -645,7 +646,7 @@
         private Label label9;
         private TextBox txtUserName;
         private ComboBox cmbUserRole;
-        private TextBox textBox8;
+        private TextBox txtUserAddress;
         private TextBox txtUserPass;
         private TextBox txtUserEmail;
     }
